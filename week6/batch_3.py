888667ed0a564ce7ee12c091f9c2921b3082e323
nevinpolat <npolat@live.co.uk>
2024-07-01 17:05:37 +0100

    Homework 6 added

---
+++ week6/batch_3.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+import pickle
+import pandas as pd
+
+
+def prepare_data(df, categorical):
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+
+    return df
+
+
+def read_data(filename, categorical):
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+    if S3_ENDPOINT_URL is not None:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+
+        df = pd.read_parquet(filename, storage_options=options)
+    else:
+        df = pd.read_parquet(filename)
+
+    return prepare_data(df, categorical)
+
+
+def main(year, month):
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f'output/taxi_type=yellow_year={year:04d}_month={month:02d}.parquet'
+
+    categorical = ['PULocationID', 'DOLocationID']
+
+    df = read_data(input_file, categorical)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+
+    print('predicted mean duration:', y_pred.mean())
+
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+
+   
+if __name__ == '__main__':
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
+
+    main(year=year, month=month)
